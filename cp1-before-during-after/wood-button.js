075a69addde34766703f761db108453743951705
clamoodle <inthemistofoodles@gmail.com>
2023-04-26 18:11:12 -0700

    lint

--- cp1-before-during-after/wood-button.js
+++ cp1-before-during-after/wood-button.js
@@ -1,27 +1,26 @@
 "use strict";
 
-(function() {
-    const SAWED_PIC = "imgs/sawed wood.jpg";
-    const BUTTON = document.querySelector("button");
+(function () {
+  const SAWED_PIC = "imgs/sawed wood.jpg";
+  const BUTTON = document.querySelector("button");
 
-    let isSawed = false;
+  let isSawed = false;
 
-    function init() {
-        BUTTON.addEventListener("click", switchWood);
+  function init() {
+    BUTTON.addEventListener("click", switchWood);
+  }
+
+  function switchWood() {
+    const woodImage = document.querySelector("img");
+    if (!isSawed) {
+      woodImage.src = SAWED_PIC;
+      woodImage.alt = "two smaller pieces of wood";
+      BUTTON.textContent = "BACK";
+      isSawed = true;
+    } else {
+      window.location.href = "index.html";
     }
+  }
 
-    function switchWood() {
-        const woodImage = document.querySelector("img");
-        if (!isSawed) {
-            woodImage.src = SAWED_PIC;
-            woodImage.alt = "two smaller pieces of wood";
-            BUTTON.textContent = "BACK";
-            isSawed = true;
-        } else {
-            window.location.href='main.html'
-        }
-    }
-
-    init();
-
+  init();
 })();
