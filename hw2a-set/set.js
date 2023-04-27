7f690ab938de4f4bbf201494b79fcd1a3880ff50
clamoodle <inthemistofoodles@gmail.com>
2023-04-27 10:51:19 -0700

    add id for filler rect

--- hw2a-set/set.js
+++ hw2a-set/set.js
@@ -1,6 +1,6 @@
 "use strict";
 
 function toggleView() {
-    sq("#game-view").classList.toggle("hidden");
-    sq("#menu-view").classList.toggle("hidden");
+  qs("#game-view").classList.toggle("hidden");
+  qs("#menu-view").classList.toggle("hidden");
 }
