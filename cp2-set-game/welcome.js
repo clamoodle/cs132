b0a049e1fde6c281b38c806a082c348a24be65a0
clamoodle <inthemistofoodles@gmail.com>
2023-04-28 18:41:20 -0700

    songs fixed

--- cp2-set-game/welcome.js
+++ cp2-set-game/welcome.js
@@ -12,7 +12,27 @@
         qs("#mute-button").addEventListener("click", startGame);
         qs("#sound-button").addEventListener("click", toggleSound);
         qs("#login-button").addEventListener("click", showLogin);
+        // For gap-less seamless looping
+        WELCOME_SONG.addEventListener('timeupdate', function(){
+            var buffer = .36
+            if(this.currentTime > this.duration - buffer){
+                this.currentTime = 0
+                this.play()
+            }
+        });
     }
+
+    // /**
+    //  * For gap-less seamless looping between each loop of a song, shifts the next play-back forward
+    //  * @param {float} gapTime - time in gap to be made up
+    //  */
+    // function shiftMusic(gapTime) {
+    //     var buffer = gapTime;
+    //     if(this.currentTime > this.duration - buffer){
+    //         this.currentTime = 0
+    //         this.play()
+    //     }
+    // }
 
     function startGame() {
         // Hides sound menu
