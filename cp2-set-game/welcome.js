c3c87a24f551dfd2e1ac23957b2639f067d5f7f3
clamoodle <inthemistofoodles@gmail.com>
2023-05-03 01:05:02 -0700

    image animation n stuff

--- cp2-set-game/welcome.js
+++ cp2-set-game/welcome.js
@@ -6,39 +6,42 @@
 (function () {
     "use strict";
     const WELCOME_SONG = qs("#welcome-song");
+    const WELCOME_SONG_TIME_SHIFT = 0.34; // The amount of time at the end of the audio track to be overlapped
 
     function init() {
         qs("#music-button").addEventListener("click", startGameWithMusic);
         qs("#mute-button").addEventListener("click", startGame);
         qs("#sound-button").addEventListener("click", toggleSound);
         qs("#login-button").addEventListener("click", showLogin);
+        qs("#create-button").addEventListener(
+            "click",
+            (open = () => {
+                window.location.href = "create-char.html";
+            })
+        );
         // For gap-less seamless looping
-        WELCOME_SONG.addEventListener('timeupdate', function(){
-            var buffer = .36
-            if(this.currentTime > this.duration - buffer){
-                this.currentTime = 0
-                this.play()
-            }
-        });
+        WELCOME_SONG.addEventListener("timeupdate", shiftMusic);
     }
 
-    // /**
-    //  * For gap-less seamless looping between each loop of a song, shifts the next play-back forward
-    //  * @param {float} gapTime - time in gap to be made up
-    //  */
-    // function shiftMusic(gapTime) {
-    //     var buffer = gapTime;
-    //     if(this.currentTime > this.duration - buffer){
-    //         this.currentTime = 0
-    //         this.play()
-    //     }
-    // }
+    /**
+     * For gap-less seamless looping between each loop of a song, shifts the next play-back forward
+     */
+    function shiftMusic() {
+        console.log(this.currentTime);
+        gapTime = evt.currentTar; // Time in gap to be made up
+        if (this.currentTime > this.duration - WELCOME_SONG_TIME_SHIFT) {
+            this.currentTime = 0;
+            this.play();
+        }
+    }
 
     function startGame() {
         // Hides sound menu
         qs("#sound-menu").classList.add("hidden");
         // Show title menu
         qs("#welcome-view").classList.remove("hidden");
+        // Resets background track
+        WELCOME_SONG.currentTime = 0;
     }
 
     function startGameWithMusic() {
@@ -56,6 +59,7 @@
 
     function showLogin() {
         qs("#login-view").classList.remove("hidden");
+        window.scrollBy(0,1);
     }
 
     init();
