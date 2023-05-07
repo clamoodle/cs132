a9bf5071999bf291587ecd35c627b8188dea5ff7
clamoodle <inthemistofoodles@gmail.com>
2023-05-06 22:30:40 -0700

    fixed animation/sound toggle bugs

--- cp2-set-game/game.js
+++ cp2-set-game/game.js
@@ -1,19 +1,27 @@
-(function() {
-    AVATAR = qs("#avatar")
-    JUMP_COOLDOWN_MS = 600; // time in MS, twice the time in game-styles.css to jump up
+(function () {
+    "use strict";
+    const JUMP_COOLDOWN_MS = 600; // time in MS, the time in game-styles.css to jump up
+    const NUM_OBSTACLES = 10;
+    // minimum and maximum time in MS between each obstacle
+    const OBSTACLE_MIN_TIME_GAP_MS = 500;
+    const OBSTACLE_MAX_TIME_GAP_MS = 3000;
 
     function init() {
         window.addEventListener("keydown", avatarControl);
+        generateMap();
     }
 
     function avatarControl(e) {
-        console.log(AVATAR.style.animationPlayState);
-        AVATAR.style.animationPlayState = "paused";
-        console.log(AVATAR.style.animationPlayState);
+        let avatar = qs("#avatar");
+
+        // console.log(AVATAR.style.animationPlayState);
+        // AVATAR.style.animationPlayState = "paused";
+        // console.log(AVATAR.style.animationPlayState);
         switch (e.keyCode) {
             // jumping case
-            case 38: case 32: // up or space key respectively
-                jump(AVATAR);
+            case 38:
+            case 32: // up or space key respectively
+                jump(avatar);
         }
         // AVATAR.style.animationPlayState = "running";
     }
@@ -21,12 +29,41 @@
     // Makes whoever the target is jump up and land down
     function jump(target) {
         if (!target.classList.contains("jump")) {
+            // target.style.animation = "jump 0.3s cubic-bezier(0.25, 0.46, 0.45, 0.94) alternate infinite";
+            // target.style.animationIterationCount = "2";
             target.classList.add("jump");
         }
-        removeJump = () => {
+        setTimeout(() => {
             target.classList.remove("jump");
+        }, JUMP_COOLDOWN_MS);
+    }
+
+    function generateMap() {
+        let sumTimeGapSoFarMS = 0;
+
+        // Generating obstacles sliding across the screen
+        for (let i = 0; i < NUM_OBSTACLES; i++) {
+            let randomTimeGapMS =
+                Math.floor(
+                    Math.random() *
+                        (OBSTACLE_MAX_TIME_GAP_MS - OBSTACLE_MIN_TIME_GAP_MS)
+                ) + OBSTACLE_MIN_TIME_GAP_MS;
+            console.log(randomTimeGapMS);
+            console.log(sumTimeGapSoFarMS);
+            setTimeout(() => {
+                const newObstacle = document.createElement("div");
+                newObstacle.classList.add("obstacle", "sliding-layer");
+                qs("#game").appendChild(newObstacle);
+           }, randomTimeGapMS + sumTimeGapSoFarMS);
+           sumTimeGapSoFarMS += randomTimeGapMS; 
         }
-        setTimeout(removeJump, JUMP_COOLDOWN_MS);
+
+        // The final goal/finish line after the obstacles
+        setTimeout(() => {
+            const goal = document.createElement("div");
+            goal.classList.add("goal", "sliding-layer");
+            qs("#game").appendChild(goal);
+       }, sumTimeGapSoFarMS);
     }
 
     init();
--- cp2-set-game/welcome.js
+++ cp2-set-game/welcome.js
@@ -7,12 +7,11 @@
     "use strict";
     const WELCOME_SONG = qs("#welcome-song");
     const WELCOME_SONG_TIME_SHIFT = 0.34; // The amount of time at the end of the audio track to be overlapped
-    const MUSIC_TOGGLE = qs("#music-toggle");
 
     function init() {
         qs("#music-button").addEventListener("click", startGameWithMusic);
         qs("#mute-button").addEventListener("click", startGame);
-        MUSIC_TOGGLE.addEventListener("click", toggleMusic);
+        qs("#music-toggle").addEventListener("click", toggleMusic);
         qs("#login-button").addEventListener("click", showLogin);
         qs("#create-button").addEventListener("click", () => {
             window.location.href = "create-char.html";
@@ -44,28 +43,24 @@
         qs("#sound-menu").classList.add("hidden");
         // Show title menu
         qs("#welcome-view").classList.remove("hidden");
-        // Resets background track
-        WELCOME_SONG.currentTime = 0;
+        
     }
 
     function startGameWithMusic() {
-        // Default is muted upon page first loading
+        // Default music is muted upon page first loading
         toggleMusic();
         startGame();
     }
 
     function toggleMusic() {
-        if (WELCOME_SONG.classList.contains("muted")) {
-            MUSIC_TOGGLE.classList.remove("muted");
-            MUSIC_TOGGLE.style.backgroundImage = url(
-                "media/music-on-button.png"
-            );
+        let musicToggle = qs("#music-toggle");
+        if (musicToggle.classList.contains("muted")) {
+            musicToggle.classList.remove("muted");
+            musicToggle.src = "media/music-on-button.png";
             WELCOME_SONG.play();
         } else {
-            MUSIC_TOGGLE.classList.add("muted");
-            MUSIC_TOGGLE.style.backgroundImage = url(
-                "media/music-off-button.png"
-            );
+            musicToggle.classList.add("muted");
+            musicToggle.src = "media/music-off-button.png";
             WELCOME_SONG.pause();
         }
     }
